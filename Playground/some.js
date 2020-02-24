386c9fe8c1a494a4bc3e3711097bb6fa5e250460
Anna Salcedo <salcedoanna8@gmail.com>
2020-02-24 18:11:22 -0500

    Learning

--- Playground/some.js
+++ Playground/some.js
@@ -1,1 +1,14 @@
-console.log('im in another file');
+// const person = {
+//   first: "ania",
+//   last: "salcedo",
+//   age: 25,
+// };
+
+let somethignUndefined; //has no value set
+const somethingNull = null; //has a value of null
+
+let isDrawing = false;
+let age = 18;
+const ofAge = age > 19;
+
+console.log(ofAge);
--- Playground/types.js
+++ Playground/types.js
@@ -10,3 +10,18 @@ const sentence = `She's so cool`;
 const hello = "Hello my name is " + name + ". Nice to meet you";
 
 const hi = `Hello my name is ${name}. Nice to meet you`;
+
+// const person = {
+//   first: "ania",
+//   last: "salcedo",
+//   age: 25,
+// };
+
+let somethignUndefined; //has no value set
+const somethingNull = null; //has a value of null
+
+let isDrawing = false;
+let age = 18;
+const ofAge = age > 19;
+
+console.log(ofAge);
