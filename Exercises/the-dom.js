4a2b6b2a09e330f734cfd7b94e78e944fe60b6ba
Anna Salcedo <salcedoanna8@gmail.com>
2020-02-28 18:44:01 -0500

    DOM attributes

--- Exercises/the-dom.js
+++ Exercises/the-dom.js
@@ -1,2 +1,15 @@
-const heading = document.querySelector("h2");
-console.log(heading);
+// const heading = document.querySelector("h2"); // w nawiasach also can be a class
+
+// console.dir(heading.innerHTML);
+//textContent shows us everything - the text and included script tags if
+//there is any vs. innerText will only show us the text that is
+//readable for a human being - it is aware of styling.
+//heading.innerHTML - includes inner HTML tags.
+//heading.outerHTML - includes outer HTML tags.
+
+//CLASSES!
+const pic = document.querySelector(`.nice`);
+pic.classList.add("dog");
+console.log(pic.classList);
+
+pic.alt = "Cute Puppy"; //for people who use screen readers.
